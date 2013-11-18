55876368c259459e3986fd93bdcec988d63771b4
Ania Tassinari <aniat@bu.edu>
2013-11-18 11:29:02 -0500

    Update R_tricks.R
    read command line arguments

--- R_tricks.R
+++ R_tricks.R
@@ -1,3 +1,17 @@
-# Tidy ugly R code
+### Tidy ugly R code
 library(formatR)
 tidy.source(source="infile.R", file="outfile.R")
+
+
+### Read arguments from the command line
+exmpl.R 2010-01-28 example 100
+
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+# trailingOnly=TRUE means that only your arguments are returned, check:
+# print(commandsArgs(trailingOnly=FALSE))
+# call args by place in list
+start_date <- as.Date(args[1])
+name <- args[2]
+n <- as.integer(args[3])
