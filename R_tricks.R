d880a7a6cff25274f928d71e79a23f92fd9f5eeb
Ania Tassinari <aniat@bu.edu>
2014-05-27 10:44:21 -0400

    add piece-wise mean for matrices

--- R_tricks.R
+++ R_tricks.R
@@ -6,7 +6,7 @@ tidy.source(source="infile.R", file="outfile.R")
 ### Read arguments from the command line
 exmpl.R 2010-01-28 example 100
 
-options(echo=TRUE) # if you want see commands in output file
+options(echo=TRUE)  # if you want to see commands in output file
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 # trailingOnly=TRUE means that only your arguments are returned, check:
@@ -15,3 +15,9 @@ print(args)
 start_date <- as.Date(args[1])
 name <- args[2]
 n <- as.integer(args[3])
+
+### Calculate an element by element average of matrices in a list (mylist)
+# ommits NA's and calculates average based on reduced sum of elements
+# ex. ave[2,5] = ave(a[2,5], b[2,5])
+library(WGCNA)
+do.call(pmean, mylist)
