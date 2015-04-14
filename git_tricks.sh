5233ab170d316fe2ea6b35bbcadb356e8d806acf
Ania Tassinari <anna.krentowicz@gmail.com>
2015-04-14 18:38:50 -0400

    diff file before commit

--- git_tricks.sh
+++ git_tricks.sh
@@ -1,2 +1,9 @@
-# Remove a tracked file from repository witout deleting it from disc
+## Remove a tracked file from repository witout deleting it from disc
 git rm --cached file
+
+## See what changes were made to a file before commiting 
+## so that you can add an informative commit message
+# If you want to see what you haven't git added yet:
+git diff -- myfile.txt
+# or if you want to see already-added changes
+git diff --cached -- myfile.txt
